580aed4b4d0d87f6928369183a8c4c8027c54d60
amaanraaj <amandwivedi00@gmail.com>
2024-02-05 22:17:40 +0530

    made chat app sending messages compatible

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useMemo, useState } from 'react';
+import './App.css';
+import {io} from 'socket.io-client';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message,setMessage] = useState("");
+  const socket = useMemo(()=> io("http://localhost:3000/"), []);
 
+  useEffect(()=>{
+    socket.on('connect',()=>{
+      console.log('conneted to client');
+    });
+    // welcome even created 
+    socket.on('welcome',(textFromServer)=>{
+      console.log(textFromServer);
+    });
+
+    socket.on('receive-message',(data)=>{
+      console.log(data);
+    })
+
+    return ()=>{
+      socket.disconnect();
+    }
+  },[]);
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    socket.emit('message', message);
+    setMessage("");
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form onSubmit={handleSubmit}>
+        <textarea value={message} onChange={(e)=>(setMessage(e.target.value))}></textarea>
+        <button type='submit'>submit</button>
+      </form>
     </>
   )
 }
--- server/index.js
+++ server/index.js
@@ -1,14 +1,38 @@
 const express = require('express');
-const {Server} = require('socket.io')
+const {createServer} = require('http');
+const {Server} = require('socket.io');
+const cors = require('cors');
 
 const app = express();
-// const server = new Server(app);
+const server = createServer(app);
 
-// const io = new Server(server);
+const io = new Server(server,{
+    cors:{
+        origin: 'http://localhost:5173',
+        methods: ['GET','POST'],
+        credentials: true
+    }
+});
 
 app.get("/",(req,res)=>{
     res.json({message:"started"});
-})
-app.listen(3000, ()=>{
+});
+
+io.on('connection',(socket)=>{
+    console.log("user connected", socket.id);
+    // socket.emit('welcome',`Hey welcome to server!! ${socket.id}`);
+    // socket.broadcast.emit('welcome',`${socket.id} joined the server!`)
+    socket.on('message',(data)=>{
+        socket.broadcast.emit('receive-message',data)
+    });
+
+
+    socket.on('disconnect',()=>{
+        console.log("User disconnected", socket.id);
+    });
+
+});
+
+server.listen(3000, ()=>{
     console.log("Server is up and running");
 })
